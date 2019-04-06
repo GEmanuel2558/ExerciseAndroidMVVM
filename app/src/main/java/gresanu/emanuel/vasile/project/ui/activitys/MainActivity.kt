8b83cabd5daa97330568e2da0065441a881fd949
Gresanu Emanuel Vasile <gresanu.emanuel.vasile@gmail.com>
2019-04-07 00:14:41 +0300

    Add main activity binder layout

--- app/src/main/java/gresanu/emanuel/vasile/project/ui/activitys/MainActivity.kt
+++ app/src/main/java/gresanu/emanuel/vasile/project/ui/activitys/MainActivity.kt
@@ -1,13 +1,17 @@
 package gresanu.emanuel.vasile.project.ui.activitys
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import dagger.android.support.DaggerAppCompatActivity
 import gresanu.emanuel.vasile.project.R
+import gresanu.emanuel.vasile.project.databinding.ActivityMainBinding
 
 class MainActivity : DaggerAppCompatActivity() {
 
+    private lateinit var currentView: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        currentView = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
