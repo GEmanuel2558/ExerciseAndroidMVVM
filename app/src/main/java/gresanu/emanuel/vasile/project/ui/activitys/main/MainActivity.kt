8cbf9c48f2812a3d145b5d8cd7d346c163f12318
Gresanu Emanuel Vasile <gresanu.emanuel.vasile@gmail.com>
2019-04-07 16:37:01 +0300

    Add save and restore calls of the current activity

--- app/src/main/java/gresanu/emanuel/vasile/project/ui/activitys/main/MainActivity.kt
+++ app/src/main/java/gresanu/emanuel/vasile/project/ui/activitys/main/MainActivity.kt
@@ -5,13 +5,31 @@ import android.os.Bundle
 import dagger.android.support.DaggerAppCompatActivity
 import gresanu.emanuel.vasile.project.R
 import gresanu.emanuel.vasile.project.databinding.ActivityMainBinding
+import gresanu.emanuel.vasile.project.extensions.getViewModel
+import gresanu.emanuel.vasile.project.ui.activitys.main.viewmodeler.MainActivityViewModel
 
 class MainActivity : DaggerAppCompatActivity() {
 
     private lateinit var currentView: ActivityMainBinding
 
+    private val viewModel by lazy {
+        getViewModel<MainActivityViewModel>(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         currentView = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        if(viewModel.isNewlyCreated && null!=savedInstanceState) {
+            viewModel.onRestoreState(savedInstanceState)
+        }
+        viewModel.isNewlyCreated = false
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState.takeIf { null != it }?.let {
+            viewModel.onSaveInstance(it)
+        }
     }
 }
--- app/src/main/java/gresanu/emanuel/vasile/project/ui/activitys/main/viewmodeler/MainActivityViewModel.kt
+++ app/src/main/java/gresanu/emanuel/vasile/project/ui/activitys/main/viewmodeler/MainActivityViewModel.kt
@@ -1,4 +1,16 @@
 package gresanu.emanuel.vasile.project.ui.activitys.main.viewmodeler
 
+import android.os.Bundle
+
 class MainActivityViewModel: BaseViewModel() {
+
+    var isNewlyCreated = true
+
+    fun onSaveInstance(outState: Bundle) {
+
+    }
+
+    fun onRestoreState(savedInstance: Bundle) {
+
+    }
 }
