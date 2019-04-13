60a54fa8343ab2e94995e04c80f90ed330080a6f
Gresanu Emanuel Vasile <gresanu.emanuel.vasile@gmail.com>
2019-04-13 23:05:24 +0300

    Add timestamp to check the freshness of the data

--- app/src/main/java/gresanu/emanuel/vasile/project/recyclers/models/Message.kt
+++ app/src/main/java/gresanu/emanuel/vasile/project/recyclers/models/Message.kt
@@ -1,3 +1,8 @@
 package gresanu.emanuel.vasile.project.recyclers.models
 
-data class Message(val id: Int=-1, val title:String="", val message:String="")
+data class Message(val id: Int=-1, val title:String="", val message:String="", @Transient val timeStamp: Long = System.currentTimeMillis()) {
+
+    private val STALE_MS = (5 * 1000).toLong() // Data is stale after 5 seconds
+
+    fun isUpToDate() = (System.currentTimeMillis() - timeStamp < STALE_MS)
+}
